cea84976b2a3d5aab86f5c65cbf762917309cec1
Bjørn Tore Svinningen <postmannen@gmail.com>
2018-07-19 13:01:17 +0200

    made a loop to choose and execute the correct template based on the msg recieved on the socket.

--- main.go
+++ main.go
@@ -86,56 +86,32 @@ func (s *server) socketHandler() http.HandlerFunc {
 			//print message to console
 			fmt.Printf("Client=%v typed : %v \n", conn.RemoteAddr(), string(msg))
 
-			//Check if the message from browser matches any of the predefined CASE
-			//claused below, and execute the the block for that CASE.
+			//loop through the map and check if there is a key in the map that match with
+			//the msg comming in on the websocket from browser.
+			//If there is no match, whats in msg will be sendt directly back ovet the socket,
+			//to be printed out in the client browser.
 			strMsg := string(msg)
-			switch strMsg {
-			case "addButton":
-				//msg = []byte("<button>Test button</button>")
-				//Create a buffer to hold all the data in the template.
-				//Since bytes.Buffer is a writer we can use it as the
-				//destination when executing the template.
-				var tplData bytes.Buffer
-				tpl.ExecuteTemplate(&tplData, "buttonTemplate1", divID)
-				d := tplData.String()
-				//New-lines between the html tags in the template source code
-				//is shown in the browser. Trimming awat the new-lines in each line
-				//in the template data.
-				d = strings.TrimSpace(d)
-				msg = []byte(d)
+			if strMsg != "" {
+				for k, v := range s.msgToTemplate {
+					if k == strMsg {
+						//Declare a bytes.Buffer to hold the data for the executed template.
+						var tplData bytes.Buffer
+						//tplData is a bytes.Buffer, which is a type io.Writer. Here we choose
+						//execute the template, but passing the output into tplData insted of
+						//'w'. Then we can take the data in tplData and send them over the socket.
+						tpl.ExecuteTemplate(&tplData, v, divID)
+						d := tplData.String()
+						//New-lines between the html tags in the template source code
+						//is shown in the browser. Trimming awat the new-lines in each line
+						//in the template data.
+						d = strings.TrimSpace(d)
+						msg = []byte(d)
+					}
+				}
 				divID++
-			case "addInput":
-				msg = []byte("<input placeholder='put something here'></input>")
-			case "addTemplate":
-				//Create a buffer to hold all the data in the template.
-				//Since bytes.Buffer is a writer we can use it as the
-				//destination when executing the template.
-				var tplData bytes.Buffer
-				tpl.ExecuteTemplate(&tplData, "socketTemplate1", divID)
-				d := tplData.String()
-				//New-lines between the html tags in the template source code
-				//is shown in the browser. Trimming awat the new-lines in each line
-				//in the template data.
-				d = strings.TrimSpace(d)
-				msg = []byte(d)
-				divID++
-			case "addParagraph":
-				//Create a buffer to hold all the data in the template.
-				//Since bytes.Buffer is a writer we can use it as the
-				//destination when executing the template.
-				var tplData bytes.Buffer
-				tpl.ExecuteTemplate(&tplData, "paragraphTemplate1", divID)
-				d := tplData.String()
-				//New-lines between the html tags in the template source code
-				//is shown in the browser. Trimming awat the new-lines in each line
-				//in the template data.
-				d = strings.TrimSpace(d)
-				msg = []byte(d)
-				divID++
-			default:
 			}
 
-			//write message back to browser
+			//write message back on the socket to the browser
 			err = conn.WriteMessage(msgType, msg)
 			if err != nil {
 				fmt.Println("error: WriteMessage failed :", err)
@@ -144,11 +120,6 @@ func (s *server) socketHandler() http.HandlerFunc {
 
 		}
 	}
-}
-
-func (s *server) chooseTemplateName(msg string) (name string) {
-
-	return
 }
 
 //The rootHandle which is like a normal handle is responsible for
